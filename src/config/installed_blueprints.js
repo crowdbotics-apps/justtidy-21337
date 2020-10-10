b30d0c9fb09da4942f720c89bce672a7e4d2c669
Crowdbotics <team@crowdbotics.com>
2020-10-10 18:27:23 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile136847', human_name: 'User Profile', access_route: 'UserProfile136847'},
 { name: 'UserProfile136781', human_name: 'User Profile', access_route: 'UserProfile136781'},
 { name: 'Tutorial136780', human_name: 'Tutorial', access_route: 'Tutorial136780', icon: 'question-circle'},
 { name: 'NotificationList136752', human_name: 'Notification List', access_route: 'NotificationList136752'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile136847Navigator from '../features/UserProfile136847/navigator';
 import UserProfile136781Navigator from '../features/UserProfile136781/navigator';
 import Tutorial136780Navigator from '../features/Tutorial136780/navigator';
 import NotificationList136752Navigator from '../features/NotificationList136752/navigator';
@@ -19,6 +20,7 @@ import UserProfile136741Navigator from '../features/UserProfile136741/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+UserProfile136847: { screen: UserProfile136847Navigator },
 UserProfile136781: { screen: UserProfile136781Navigator },
 Tutorial136780: { screen: Tutorial136780Navigator },
 NotificationList136752: { screen: NotificationList136752Navigator },
